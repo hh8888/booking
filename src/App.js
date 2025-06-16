98bafd416583f88cc7b29beca2ea4dddc80507d4
Harry Meng <harry_gx@hotmail.com>
2025-06-16 23:05:41 +1000

    debug reset pw

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from './Auth';
 import Services from './Services';
 import Bookings from './Bookings';
