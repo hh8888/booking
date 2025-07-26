9a5cc6da9d817a80eb2539f1283d444de81bffd0
Harry Meng <harry_gx@hotmail.com>
2025-07-26 21:27:34 +1000

    updated font size

--- src/components/common/LocationSelector.js
+++ src/components/common/LocationSelector.js
@@ -94,14 +94,14 @@ const LocationSelector = () => {
 
   return (
     <div className="mt-2">
-      <label htmlFor="location-select" className="text-sm font-medium text-gray-700 mr-2">
+      <label htmlFor="location-select" className="text-base font-bold text-gray-700 mr-2">
         {t('common.location')}
       </label>
       <select
         id="location-select"
         value={selectedLocationIndex || ''} // Handle null case
         onChange={(e) => handleLocationChange(e.target.value)}
-        className="text-sm border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
+        className="text-base border-2 border-blue-500 rounded-md shadow-sm focus:border-blue-600 focus:ring-blue-500"
       >
         {locations.map((location) => (
           <option key={location.id} value={location.id}>
