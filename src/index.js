bc4d1f8fff273c0435f3f979b90438df4bca5e23
Harry Meng <harry_gx@hotmail.com>
2025-11-13 19:57:37 +1100

    previous commit seems fiex the pw rest, this commit is to fix refresh looping

--- src/index.js
+++ src/index.js
@@ -3,30 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// Remove or comment out the service worker import
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
 
-// Remove all service worker registration code
-// serviceWorkerRegistration.register({
-//   onUpdate: (registration) => {
-//     if (window.confirm('New version available! Click OK to update.')) {
-//       if (registration && registration.waiting) {
-//         registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-//         window.location.reload();
-//       }
-//     }
-//   },
-//   onSuccess: (registration) => {
-//     console.log('SW registered: ', registration);
-//   }
-// });
-
-// serviceWorkerRegistration.unregister();
+// By unregistering the service worker, we ensure that the browser fetches the latest version of the app,
+// preventing potential issues with cached, outdated code that could cause infinite loops or other bugs.
+serviceWorkerRegistration.unregister();
 
 reportWebVitals();
 
